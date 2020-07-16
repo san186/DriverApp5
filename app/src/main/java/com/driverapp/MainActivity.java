96f5aae8601a7235e7406d272ab68335e1e6c6f1
san186 <sanmiphone@gmail.com>
2020-07-16 21:13:53 +0530

    Vj EditText commit

--- app/src/main/java/com/driverapp/MainActivity.java
+++ app/src/main/java/com/driverapp/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.et_name);
     }
 }
