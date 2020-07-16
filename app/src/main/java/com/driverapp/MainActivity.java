f22b77387a7812ccd497b563b1f5e0f974fac2d6
Santhosh <sanmiphone@gmail.com>
2020-07-16 21:00:19 +0530

    Santhosh Commit two

--- app/src/main/java/com/driverapp/MainActivity.java
+++ app/src/main/java/com/driverapp/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+
+    EditText edt;
+TextView Txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        Txt = findViewById(R.id.textView);
+
+
     }
 }
